702fcbd34fb8258b1a36c008b4a49283fff200f5
Mateus Okt <106376554+MateusVeloso16@users.noreply.github.com>
2024-02-25 23:42:56 +0000

    Set the second class as an Inner class

--- CA2MateusVeloso.java
+++ CA2MateusVeloso.java
@@ -1,72 +1,67 @@
 package ca2mateusveloso;
 
-public class Employee { //can be accessed outside the package since its public.
+public class Employee {
 
     String name;
     String email;
     int empNum;
     static int nextEmpNum = 1;
 
-
     public Employee() {
-        this.name = "John Doe";
-        this.email = "john@example.com";
-        this.empNum = nextEmpNum++;
+        this("Jhow Jhow", "jhow@example.com");
     }
-     
+
     public Employee(String name, String email) {
         this.name = name;
         this.email = email;
         this.empNum = nextEmpNum++;
-
     }
-    
+
     public String getName(){
         return name;
     }
-    
+
     public String getEmail(){
-        
         if (email.length() > 3) {
-        } 
+        }
         return email;
     }
-    
-    public int getNextEmpNum(){
-        return nextEmpNum;
+
+    public int getEmpNum(){
+        return empNum;
     }
 
     public static void main(String[] args) {
+        Employee employee = new Employee();
 
-        Employee defaultEmployee = new Employee();
+        System.out.println("Employee: " + employee.getName() + ", " + employee.getEmail());
+        System.out.println(Employee.nextEmpNum);
 
+        EmployeeTest employeeTest = employee.new EmployeeTest();
 
-        Employee parametersEmployee = new Employee("Jane Doe", "jane@example.com");
-        
-        
-        
+        employeeTest.main(args);
     }
-    
-    
-    
-}
 
-class EmployeeTest{
-
-    public static void main (String[] args){
-    
-        Employee[] projectGroup = new Employee[3]; // Declare an array to store 3 Employee objects
+    class EmployeeTest {
+        public void main(String[] args) {
+            Employee[] projectGroup = new Employee[3];
 
             projectGroup[0] = new Employee("Joe Bloggs", "jb@gmail.com");
             projectGroup[1] = new Employee("Ann Banana", "ab@gmail.com");
             projectGroup[2] = new Employee("Tom Thumb", "tt@gmail.com");
-            
-            System.out.println("Next Employee Number: " + Employee.nextEmpNum);
-            
-    }
-    
-    
-    
-}
 
-    
+            for (int i = 0; i < projectGroup.length; i++) {
+                System.out.println("Employee " + (i+1) + ": " + projectGroup[i].getName() + ", " + projectGroup[i].getEmail());
+            }
+
+            int m = 0;
+
+            System.out.println("Employees with Employee Number > " + m + ":");
+            for (Employee employee : projectGroup) {
+                if (employee.getEmpNum() > m) {
+                    System.out.println(employee.getName());
+                }
+            }
+        }
+    }
+}
