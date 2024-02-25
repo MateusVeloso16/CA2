e2ab780af607b08bca5a73e29fcf964e6ca9b8bc
Mateus Okt <106376554+MateusVeloso16@users.noreply.github.com>
2024-02-25 16:08:52 +0000

    Implementing accessor methods Update

--- CA2MateusVeloso.java
+++ CA2MateusVeloso.java
@@ -5,15 +5,25 @@ public class Employee {
     String name;
     String email;
     int empNum;
+    static int nextEmpNum = 1;
+
 
     public Employee() {
         this.name = "John Doe";
         this.email = "john@example.com";
+        this.empNum = nextEmpNum++;
+
     }
 
     public Employee(String name, String email) {
         this.name = name;
         this.email = email;
+        this.empNum = nextEmpNum++;
+
+    }
+    
+    public String getName(){
+        return name;
     }
 
     public static void main(String[] args) {
