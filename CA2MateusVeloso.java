c16f9608df832d786ad69a55436f3800da1364d8
Mateus Okt <106376554+MateusVeloso16@users.noreply.github.com>
2024-02-26 22:46:11 +0000

    Tryng to implement the console menu in netBeans but for some reason its not working. Very sad :(

--- CA2MateusVeloso.java
+++ CA2MateusVeloso.java
@@ -2,6 +2,7 @@ package ca2mateusveloso;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Employee {
 
@@ -80,8 +81,68 @@ public class Employee {
         String companyName = "Gnomes Ltd";
         String name;
         String email;
+        String managerUsername;
+        String managerPassword;
         ArrayList<Employee> staff = new ArrayList<>();
         
+                
+        class ConsoleMenu {
+            
+                Scanner scanner;
+
+                ConsoleMenu() {
+                    scanner = new Scanner(System.in);
+                }
+
+                public void start() {
+                    System.out.println(" Manager Username & Password");
+                    System.out.print("Username: ");
+                    String username = scanner.nextLine();
+                    System.out.print("Password: ");
+                    String password = scanner.nextLine();
+
+                    if (username.equals("Gnomeo") && password.equals("smurf")) {
+                        System.out.println("Logged");
+                        boolean loggedIn = true;
+
+                        while (loggedIn) {
+                            System.out.println("\n Menu");
+                            System.out.println("1- Acess staff");
+                            System.out.println("2- Adicionar new staff");
+                            System.out.println("3- Leave page");
+
+                            System.out.print("Select your option from 1 to 3: ");
+                            int choice = scanner.nextInt();
+
+                            switch (choice) {
+                                case 1:
+                                    listEmployees(0);
+                                    break;
+                                case 2:
+                                    scanner.nextLine();
+                                    System.out.print("Insert staff name: ");
+                                    String name = scanner.nextLine();
+                                    System.out.print("Insert staff email: ");
+                                    String email = scanner.nextLine();
+                                    addNewStaff(name, email);
+                                    System.out.println("Staff added.");
+                                    break;
+                                case 3:
+                                    System.out.println("Leaving");
+                                    loggedIn = false;
+                                    break;
+                                default:
+                                    System.out.println("Wrong. Select from 1 to 3.");
+                            }
+                        }
+                    } else {
+                        System.out.println("Fail, please insert valid password and username");
+                    }
+                }   
+        }
+        
+        
+               
         public Company() {
             this("Gnomes Ltd", "123@gmail.com");
         }
@@ -95,6 +156,22 @@ public class Employee {
             this.email = email;
         }
         
+        public String getManagerUsername() {
+            return managerUsername;
+        }
+
+        public void setManagerUsername(String managerUsername) {
+            this.managerUsername = managerUsername;
+        }
+
+        public String getManagerPassword() {
+            return managerPassword;
+        }
+
+        public void setManagerPassword(String managerPassword) {
+            this.managerPassword = managerPassword;
+        }
+                
         public void addNewStaff(String name, String email) {
             Employee employee = new Employee(name, email);
             staff.add(employee);
@@ -105,15 +182,16 @@ public class Employee {
         }
         
         public void listEmployees(int empNumGiv) {
+            
             Iterator<Employee> iterator = staff.iterator();
             System.out.println("Employees with Employee Number > " + empNumGiv + ":");
                 while (iterator.hasNext()) {
                     Employee employee = iterator.next();
-                if (employee.getEmpNum() > empNumGiv) {
-                    System.out.println(employee.getName());
-            }
+                    if (employee.getEmpNum() > empNumGiv) {
+                        System.out.println(employee.getName());
+                    }
+                }
         }
-    }
         
        
     
@@ -132,5 +210,6 @@ public class Employee {
                 System.out.println();
             }
         }
+            
     }
 }
